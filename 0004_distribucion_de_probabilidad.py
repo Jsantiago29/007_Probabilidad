88eb09bcbb44cf76c3bd38e206e52bf59f7cb88f
Jsantiago29 <125232840+Jsantiago29@users.noreply.github.com>
2023-05-12 21:15:05 -0600

    .

---
+++ 0004_distribucion_de_probabilidad.py
@@ -0,0 +1,15 @@
+
+
+import numpy as np
+
+# Parámetros de la distribución normal
+media = 0  # Media de la distribución
+desviacion = 1  # Desviación estándar de la distribución
+tamano_muestra = 10  # Tamaño de la muestra
+
+# Generación de la muestra utilizando la distribución normal
+muestra = np.random.normal(media, desviacion, tamano_muestra)
+
+# Imprimir la muestra generada
+print("Muestra generada:")
+print(muestra)
